7e1562465096cc4b3e6345e192fd2af897a7d182
hu <laopo_one@outlook.com>
2023-01-27 18:41:48 +0800

    Initial commit

--- src/main/java/com/hu/reggie/service/SetmealService.java
+++ src/main/java/com/hu/reggie/service/SetmealService.java
@@ -4,6 +4,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.hu.reggie.dto.SetmealDto;
 import com.hu.reggie.entity.Setmeal;
 
+import java.util.List;
+
 public interface SetmealService extends IService<Setmeal> {
     void saveWithDish(SetmealDto setmealDto);
+
+    void removeWithDish(List<Long> ids);
 }
